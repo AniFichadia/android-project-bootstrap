e7b18d9b32d988957ef2ad33f56a9f016692535f
Aniruddh Fichadia <Ani.Fichadia@gmail.com>
2020-10-18 13:06:34 +1100

    Sets up separate module for bootstrapping SDKs

---
+++ bootstrap_sdk_networking/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    compileSdkVersion(CommonAndroidConfig.compileSdkVersion)
+
+    defaultConfig {
+        minSdkVersion(CommonAndroidConfig.minSdkVersion)
+        targetSdkVersion(CommonAndroidConfig.targetSdkVersion)
+
+        versionCode = 1
+        versionName = "1.0"
+
+        consumerProguardFile("consumer-rules.pro")
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isMinifyEnabled = false
+        }
+
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+
+    packagingOptions {
+        exclude("META-INF/AL2.0")
+        exclude("META-INF/LGPL2.1")
+    }
+}
+
+dependencies {
+    Deps.kotlin(this)
+    Deps.logging(this)
+    Deps.networkingStack(this)
+    Deps.androidAnnotations(this)
+
+    Deps.unitTest(this)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
 rootProject.name = "android-project-bootstrap"
 
 include(":bootstrap_app")
+include(":bootstrap_sdk_networking")
