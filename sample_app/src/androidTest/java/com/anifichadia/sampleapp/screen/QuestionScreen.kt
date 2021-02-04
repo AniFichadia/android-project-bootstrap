b6578a49f2fff8e583534f8894124b081cbe2b60
Aniruddh Fichadia <Ani.Fichadia@gmail.com>
2021-02-04 22:54:11 +1100

    Updates Question screen to use Barista

--- sample_app/src/androidTest/java/com/anifichadia/sampleapp/screen/QuestionScreen.kt
+++ sample_app/src/androidTest/java/com/anifichadia/sampleapp/screen/QuestionScreen.kt
@@ -1,18 +1,14 @@
 package com.anifichadia.sampleapp.screen
 
-import android.view.View
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.RootMatchers.isDialog
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
-import com.anifichadia.bootstrap.testing.ui.testframework.espresso.EspressoOperations.onViewAfterScroll
-import com.anifichadia.bootstrap.testing.ui.testframework.espresso.matcher.RecyclerViewPositionMatcher.Companion.atRecyclerViewPosition
-import com.anifichadia.bootstrap.testing.ui.testframework.espresso.matcher.RecyclerViewSizeMatcher.Companion.recyclerViewWithSize
 import com.anifichadia.sampleapp.R
-import org.hamcrest.Matcher
-import org.hamcrest.core.AllOf.allOf
+import com.schibsted.spain.barista.assertion.BaristaListAssertions.assertDisplayedAtPosition
+import com.schibsted.spain.barista.assertion.BaristaListAssertions.assertListItemCount
+import com.schibsted.spain.barista.interaction.BaristaListInteractions.clickListItem
 
 // NOTE! Using an internal constructor so the only way to initialise QuestionScreen is through the method below
 class QuestionScreen internal constructor() {
@@ -22,8 +18,7 @@ class QuestionScreen internal constructor() {
     // NOTE! Interactions should be named semantically. Don't describe the action (eg. "tap", "click"), describe more of the outcome (eg. user "selects")
     //region Interactions
     fun selectPotentialAnswer(position: Int) {
-        onViewAfterScroll(potentialAnswer(position))
-            .perform(click())
+        clickListItem(R.id.multiple_choice_question_recyclerview_answers, position)
     }
     //endregion
 
@@ -31,32 +26,25 @@ class QuestionScreen internal constructor() {
     // NOTE! Assertions prefixed with "assert"
     //region Assertions
     fun assertNumberOfPotentialAnswers(size: Int) {
-        onView(potentialAnswersList())
-            .check(matches(recyclerViewWithSize(size)))
+        assertListItemCount(R.id.multiple_choice_question_recyclerview_answers, size)
     }
 
     fun assertPotentialAnswerText(position: Int, text: String) {
-        onViewAfterScroll(potentialAnswer(position))
-            .check(matches(withText(text)))
+        assertDisplayedAtPosition(
+            R.id.multiple_choice_question_recyclerview_answers,
+            position,
+            R.id.list_item_multiple_choice_quiz_answer_btn_answer,
+            text
+        )
     }
 
     fun assertCorrectAnswerConfirmationShown() {
+        // Note: barista doesn't have dialog assertions?
         onView(withId(R.id.alertTitle))
             .inRoot(isDialog())
             .check(matches(withText(R.string.multiple_choice_quiz_answered_correctly)))
     }
     //endregion
-
-
-    private companion object {
-        fun potentialAnswer(position: Int): Matcher<View> =
-            allOf(
-                atRecyclerViewPosition(R.id.multiple_choice_question_recyclerview_answers, position),
-                withId(R.id.list_item_multiple_choice_quiz_answer_btn_answer),
-            )
-
-        fun potentialAnswersList(): Matcher<View> = withId(R.id.multiple_choice_question_recyclerview_answers)
-    }
 }
 
 fun questionScreen(block: QuestionScreen.() -> Unit) {
